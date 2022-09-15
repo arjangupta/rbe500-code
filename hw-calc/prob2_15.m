78d3ac23f0aaba59a76cec96ce4c68927c628ab5
Arjan Gupta <arjangupta95@gmail.com>
2022-09-14 23:27:38 -0500

    Add problem 2.15 calculation

---
+++ hw-calc/prob2_15.m
@@ -0,0 +1,8 @@
+% This is the calculation code for problem 2.15 of the RBE500 textbook (HW 1)
+
+clear; close all; clc;
+
+R_1_2 = [1 0 0; 0 1/2 -sqrt(3)/2; 0 sqrt(3)/2 1/2];
+R_1_3 = [0 0 -1; 0 1 0; 1 0 0];
+
+R_1_2' * R_1_3
