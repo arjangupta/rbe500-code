8290852390daaee06e5a4e393c62b26a3d658ad4
Arjan Gupta <arjangupta95@gmail.com>
2022-09-21 00:18:46 -0500

    Write the EulerToQuat class

--- ros2-code/src/euler_to_quat/euler_to_quat_subscriber.py
+++ ros2-code/src/euler_to_quat/euler_to_quat_subscriber.py
@@ -1,10 +1,29 @@
 import rclpy
 import std_msgs # (TODO: remove this comment) for using Float32MultiArray.msg
 
+class EulerToQuat(rclpy.node.Node):
+    def __init__(self) -> None:
+        super().__init__('euler_to_quat')
+        self.subscription = self.create_subscription(
+            std_msgs.msg.Float32MultiArray.msg,
+            'euler_to_quat_topic',
+            self.subscription_callback)
+
+    def subscription_callback(self, msg):
+        print("Hi, this is the euler_to_quat package!")
+
+
 def main():
     # Initialize the ros2 client library
     rclpy.init()
-    print("Hi, this is the euler_to_quat package!")
+    # Instantiate the EulerToQuat class
+    euler_to_quat = EulerToQuat()
+    # Spin the client
+    rclpy.spin(euler_to_quat)
+
+    # Upon Ctrl+C, clean up
+    euler_to_quat.destroy_node()
+    rclpy.shutdown()
 
 if __name__ == '__main__':
     main()
