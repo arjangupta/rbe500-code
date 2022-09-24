d02e3f7ad0eaf92a346c547f95b95944a7df1963
Arjan Gupta <arjangupta95@gmail.com>
2022-09-23 23:30:23 -0500

    Make the publish script use args for the input angles

--- ros2-code/src/euler_to_quat/publish.sh
+++ ros2-code/src/euler_to_quat/publish.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-ros2 topic pub --once euler_to_quat_topic std_msgs/msg/Float32MultiArray "{data: [1.12, 57.34, 98.2]}"
+ros2 topic pub --once euler_to_quat_topic std_msgs/msg/Float32MultiArray "{data: [$1, $2, $3]}"
