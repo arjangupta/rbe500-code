45a68a68c68f7bd0ac55aed753b10dbabfd90cf9
Arjan Gupta <arjangupta95@gmail.com>
2022-09-26 03:28:01 -0500

    Edit the DH_3_10 matrix to compute A2 of 3.6

--- hw2/dh_3_10_mat.m
+++ hw2/dh_3_10_mat.m
@@ -1,14 +1,14 @@
 clear; close all; clc;
 
-syms d1 d2;
+syms d1 d2 d3;
 
 alpha = pi/2;
 a = 0;
-theta = 0;
-d = d1;
+theta = pi/2;
+d = d2;
 
-DH_3_10 = [cos(theta) -sin(theta)*cos(alpha) sin(theta)*sin(alpha) a*cos(theta); ...
-           sin(theta) round(cos(theta)*cos(alpha),2) -cos(theta)*sin(alpha) a*sin(theta); ...
+DH_3_10 = [round(cos(theta),2) round(-sin(theta)*cos(alpha),2) sin(theta)*sin(alpha) a*cos(theta); ...
+           sin(theta) round(cos(theta)*cos(alpha),2) round(-cos(theta)*sin(alpha),2) a*sin(theta); ...
            0 sin(alpha) round(cos(alpha), 2) d; ...
            0 0 0 1];
 
