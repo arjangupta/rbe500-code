8d08272cf711b9f949b378d2114b86df261ad5cc
Arjan Gupta <arjangupta95@gmail.com>
2022-09-26 03:26:10 -0500

    Edit the DH_3_10 matrix to round to nearest decimal when necessary

--- hw2/dh_3_10_mat.m
+++ hw2/dh_3_10_mat.m
@@ -1,4 +1,3 @@
-
 clear; close all; clc;
 
 syms d1 d2;
@@ -8,7 +7,9 @@ a = 0;
 theta = 0;
 d = d1;
 
-DH_3_10 = [cos(theta) -sin(theta)*cos(alpha) sin(theta)*sin(alpha) a*cos(theta);
-           sin(theta) cos(theta)*cos(alpha) -cos(theta)sin(alpha) a*sin(theta);
-           0 sin(alpha) cos(alpha) d;
-           0 0 0 1]
+DH_3_10 = [cos(theta) -sin(theta)*cos(alpha) sin(theta)*sin(alpha) a*cos(theta); ...
+           sin(theta) round(cos(theta)*cos(alpha),2) -cos(theta)*sin(alpha) a*sin(theta); ...
+           0 sin(alpha) round(cos(alpha), 2) d; ...
+           0 0 0 1];
+
+disp(DH_3_10)
