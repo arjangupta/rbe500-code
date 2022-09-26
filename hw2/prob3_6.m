26c1c76aa6a4d0d26023043f9242763c7140c9e4
Arjan Gupta <arjangupta95@gmail.com>
2022-09-26 03:42:06 -0500

    Add the code for 3.6

---
+++ hw2/prob3_6.m
@@ -0,0 +1,16 @@
+% Calculation code for problem 3.6 of the RBE500 textbook (HW 2)
+
+clear; close all; clc;
+
+syms d1 d2 d3;
+A1 = [1 0 0 0; 0 0 -1 0; 0 1 0 d1; 0 0 0 1];
+A2 = [0 0 1 0; 1 0 0 0; 0 1 0 d2; 0 0 0 1];
+A3 = [1 0 0 0; 0 1 0 0; 0 0 1 d3; 0 0 0 1];
+
+T = A1*A2*A3;
+
+% Show output matrix
+disp(T)
+
+% Generate LaTex code
+latex(T)
