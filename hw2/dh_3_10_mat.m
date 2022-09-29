a4770630c7cfc80e1eca2c8bda38b7368b932bcd
Arjan Gupta <arjangupta95@gmail.com>
2022-09-29 00:53:55 -0500

    Add some more symbolics to the dh310 script

--- hw2/dh_3_10_mat.m
+++ hw2/dh_3_10_mat.m
@@ -1,11 +1,11 @@
 clear; close all; clc;
 
-syms theta1 d1 d2 d3;
+syms theta1 theta2 a2 d1 d2 d3;
 
-alpha = -pi/2;
-a = 0;
-theta = theta1;
-d = d1;
+alpha = 0;
+a = a2;
+theta = theta2;
+d = 0;
 
 % DH_3_10 = [round(cos(theta),2) round(-sin(theta)*cos(alpha),2) sin(theta)*sin(alpha) a*cos(theta); ...
 %            sin(theta) round(cos(theta)*cos(alpha),2) round(-cos(theta)*sin(alpha),2) a*sin(theta); ...
