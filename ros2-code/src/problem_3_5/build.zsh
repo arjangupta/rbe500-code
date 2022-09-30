bc19012504392f52ef7503e469839a50279039b6
Arjan Gupta <arjangupta95@gmail.com>
2022-09-29 21:03:44 -0500

    Improve the build message for problem 3-5 ros package

--- ros2-code/src/problem_3_5/build.zsh
+++ ros2-code/src/problem_3_5/build.zsh
@@ -2,6 +2,6 @@
 
 echo "Installing dependencies..."
 rosdep install -i --from-path src --rosdistro humble -y
-echo "Building euler_to_quat..."
+echo "Building package problem_3_5..."
 colcon build --packages-select problem_3_5
 echo "Done!"
