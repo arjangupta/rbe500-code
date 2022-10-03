929c55daf9a82fd5340737393b6babab7bbde666
Arjan Gupta <arjangupta95@gmail.com>
2022-10-02 23:05:24 -0500

    Write the computations for theta1, theta2, theta3

--- ros2-code/src/homework4_ros/homework4_ros/double_subscriber_node.py
+++ ros2-code/src/homework4_ros/homework4_ros/double_subscriber_node.py
@@ -35,15 +35,24 @@ class BidirectionalKinematics(rclpy.node.Node):
             15
         )
 
+    """
+    The robot Problem 3.5 is a three-link articulated robot with only
+    three constants in its set up: d1, a2, and a3. Let us defined these
+    as class variables for this node 
+    """
+    d1 = 1
+    a2 = 1
+    a3 = 1
+
     def calculate_forward_kinematics(self, theta1, theta2, theta3):
         """
         This function calculates the forward kinematics equations
         for Problem 3.5 of the RBE 500 Main Textbook.
         """
         # First, assign values for a2, a3, d1
-        a2 = 1
-        a3 = 1
-        d1 = 1
+        a2 = self.a2
+        a3 = self.a3
+        d1 = self.d1
 
         # Now calculate sines and cosines using the revolute joint variables
         c1 = math.cos(theta1)
@@ -94,14 +103,38 @@ class BidirectionalKinematics(rclpy.node.Node):
         q1 = orientation.x
         q2 = orientation.y
         q3 = orientation.z
-        # Here, T_0_3 indicates 0 in the super script, 3 in the subscript. So, it is the homogeneous 
+        # Declare x_c, y_c, z_c to denote the position of the wrist center, just like the way it
+        # is described in our main textbook.
+        x_c = position.x
+        y_c = position.y
+        z_c = position.z
+        # T_0_3 indicates 0 in the super script, 3 in the subscript. So, it is the homogeneous 
         # transformation that gives the position and orientation of frame 3 with respect to 0. 
         T_0_3 = np.array(
-            [[(q0*q0 + q1*q1 - q2*q2 - q3*q3), 2*(q1*q2 - q0*q3),               2*(q0*q2 + q1*q3),               position.x],
-             [2*(q1*q2 + q0*q3),               (q0*q0 - q1*q1 + q2*q2 - q3*q3), 2*(q2*q3 - q0*q1),               position.y],
-             [2*(q1*q3 - q0*q2),               2*(q0*q1 + q2*q3),               (q0*q0 - q1*q1 - q2*q2 + q3*q3), position.z],
+            [[(q0*q0 + q1*q1 - q2*q2 - q3*q3), 2*(q1*q2 - q0*q3),               2*(q0*q2 + q1*q3),               x_c],
+             [2*(q1*q2 + q0*q3),               (q0*q0 - q1*q1 + q2*q2 - q3*q3), 2*(q2*q3 - q0*q1),               y_c],
+             [2*(q1*q3 - q0*q2),               2*(q0*q1 + q2*q3),               (q0*q0 - q1*q1 - q2*q2 + q3*q3), z_c],
              [0,                               0,                               0,                               1]])
-        print(f"The wrist center T matrix is given by\n{T_0_3}")
+        print(f"The wrist center T matrix is given by\n{T_0_3}\n")
+
+        # Compute theta1, theta2, and theta3 as given in our book.
+        # For theta1, we use equations 5.18 and 5.19 of our textbook
+        theta1_option1 = math.atan2(x_c, y_c)
+        theta1_option2 = math.pi + theta1_option1
+        print(f"For theta1, the two options are {theta1_option1} and {theta1_option2}")
+        # Before computing theta2 and theta3, compute r and s, which are
+        # defined right before equation 5.25 of our textbook.
+        r = x_c**2 + y_c**2
+        s = z_c - self.d_1
+        # Use equation 5.25 of textbook to find theta3
+        D = (r**2 + s**2 - (self.a2)**2 - (self.a3)**2)/(2*self.a2*self.a3)
+        theta3_option1 = math.atan2(D, math.sqrt(1 - D**2))
+        theta3_option2 = math.atan2(D, -1*math.sqrt(1 - D**2))
+        # Use equation 5.26 of textbook to find theta2
+        theta2_option1 = math.atan2(r, s) - math.atan2(self.a2 + self.a3*math.cos(theta3_option1), self.a3*math.sin(theta3_option1))
+        theta2_option2 = math.atan2(r, s) - math.atan2(self.a2 + self.a3*math.cos(theta3_option2), self.a3*math.sin(theta3_option2))
+        print(f"For theta2, the two options are {theta2_option1} and {theta2_option2}")
+        print(f"For theta3, the two options are {theta3_option1} and {theta3_option2}")
     
     def inverse_kinematics_callback(self, msg):
         """
