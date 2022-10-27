f480d2d899241f020096aaf066fe60beb465394c
Arjan Gupta <arjangupta95@gmail.com>
2022-10-27 12:14:40 -0500

    Add matlab script for problem 3

---
+++ midterm/prob3_midterm.m
@@ -0,0 +1,8 @@
+% Calculation code for problem 2 of the RBE500 Midterm
+
+clear; close all; clc;
+
+R01 = [0.7071 0 0.7071; 0 1 0; -0.7071 0 0.7071];
+R02 = [0 0.866 0.5; 0 0.5 -0.866; -1 0 0];
+
+R12 = R01'*R02
