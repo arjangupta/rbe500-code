786dd03f839715c25cd45e8b30c58d581290241a
Arjan Gupta <arjangupta95@gmail.com>
2022-11-03 09:05:37 -0500

    Begin working on hw6 simulink model

---
+++ hw6/hw6.m
@@ -0,0 +1,15 @@
+% Code for HW6 of RBE500
+
+clear; close all; clc;
+
+% System model
+J = 2;
+B = 0.5;
+
+% Controller
+K_p = 8;
+K_d = 7.5;
+
+% Simulation parameters
+sim_duration = 20;
+sampling_time = 0.01;
